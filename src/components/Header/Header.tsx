eac8c049f53e655e11bef569c33e2ebde1ca84e3
Pavel Seduan <sphreofdie@gmail.com>
2024-05-28 06:27:26 +0300

    v5

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useUser } from "@auth0/nextjs-auth0/client";
-import styles from '../../styles/Header.module.css';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../store/store';
+import styles from "../../styles/Header.module.css";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 
 const Header: React.FC = () => {
   const { user } = useUser();
@@ -12,29 +12,67 @@ const Header: React.FC = () => {
 
   return (
     <header className={styles.header}>
-      <a href="/" className={styles.logo}></a>
-      <div>
-        <Link href="/wheel">Wheel</Link>
+      <div >
+        <Link href="/"><div className={styles.logo}></div></Link>
       </div>
-      <div className={styles.userInfo}>
-        {!!user ? (
-          <>
-            <div className={styles.userProfile}>
-              <Image src={user.picture ?? defaultPicture} height={50} width={50} alt="User Picture" />
-              <span className={styles.userName}>{user.name}</span>
-            </div>
-            <div className={styles.balance}>Balance: ${balance}</div>
-            <Link href="/api/auth/logout" className={styles.link}>
-              <button className={styles.logout}></button>
+
+      <div className={styles.head}>
+
+        <div className={styles.top_header}>
+          
+          <ul className={styles.section__list}>
+            <li>Алгоритм честной игры</li>
+            <li>ЧАВО</li>
+            <li>Условия конфиденциальности</li>
+            <li>Terms of Service</li>
+            <li>Support</li>
+            <li>Блог</li>
+            <li>Бонус</li>
+          </ul>
+        </div>
+        <div className={styles.bot_header}>
+          
+            <Link href="/wheel">
+              <Image
+                src="/wheel/wheel.png"
+                width={65}
+                height={65}
+                alt="Wheel"
+              />
             </Link>
-          </>
-        ) : (
-          <Link href="/api/auth/login" className={styles.link}>
-            <button className={styles.login}>Login</button>
-          </Link>
-        )}
+            <Link href="/">
+              <Image
+                src="/icons/golden.png"
+                width={65}
+                height={65}
+                alt="Roulette"
+              />
+            </Link>
+            {!!user ? (
+              <>
+                <div className={styles.userProfile}>
+                  <Image
+                    src={user.picture ?? "/defaultPicture.jpg"}
+                    height={50}
+                    width={50}
+                    alt="User Picture"
+                  />
+                  <span className={styles.userName}>{user.name}</span>
+                </div>
+                <div className={styles.balance}>Balance: ${balance}</div>
+                <Link href="/api/auth/logout" className={styles.link}>
+                  <button className={styles.logout}></button>
+                </Link>
+              </>
+            ) : (
+              <Link href="/api/auth/login" className={styles.link}>
+                <button className={styles.login}>Login</button>
+              </Link>
+            )}
+
+       
+        </div>
       </div>
-    
     </header>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-// _app.tsx
 
 import { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
@@ -6,13 +5,21 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import store, { persistor } from '../store/store';
 import '../styles/globals.css';
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({
+  weight: ['400', '700'], 
+  subsets: ['latin'], 
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <Component {...pageProps} />
+          <div className={montserrat.className}>
+            <Component {...pageProps} />
+          </div>
         </PersistGate>
       </Provider>
     </UserProvider>
