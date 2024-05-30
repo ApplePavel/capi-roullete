89185fe07ff9a4f61bac51d291b3b7a73bc29ab8
Pavel Seduan <sphreofdie@gmail.com>
2024-05-30 05:37:08 +0300

    v8

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+      runtime: 'experimental-edge',
+    },
     reactStrictMode: true,
     images: {
       domains: ['s.gravatar.com', 'lh3.googleusercontent.com'],
